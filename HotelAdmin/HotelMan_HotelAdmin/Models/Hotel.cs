58b14cd37686b00ec5f21ecd58c2af82f3491f90
vsmross <developer.netcore88@gmail.com>
2026-06-21 17:11:56 +0530

    Remove unnecessary line in Hotel model

--- HotelAdmin/HotelMan_HotelAdmin/Models/Hotel.cs
+++ HotelAdmin/HotelMan_HotelAdmin/Models/Hotel.cs
@@ -7,7 +7,6 @@ public class Hotel
 {
     [DynamoDBHashKey("userid")] public string? UserId { get; set; }
     [DynamoDBRangeKey("id")] public string? Id { get; set; }
-
     public string? Name { get; set; }
     public int Price { get; set; }
     public int Rating { get; set; }
