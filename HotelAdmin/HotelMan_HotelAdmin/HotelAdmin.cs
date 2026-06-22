ce50fe45ccfb6e26f0c91b46b69f520a7a032b42
vsmross <developer.netcore88@gmail.com>
2026-06-22 09:28:50 +0000

    Add comment for pipeline trigger
    Added a comment to trigger a pipeline.

--- HotelAdmin/HotelMan_HotelAdmin/HotelAdmin.cs
+++ HotelAdmin/HotelMan_HotelAdmin/HotelAdmin.cs
@@ -19,6 +19,7 @@ using HttpMultipartParser;
 
 [assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
 
+// change this for dummy commit to trigger a pipeline
 namespace HotelMan_HotelAdmin;
 
 // Main Hotel admin class
